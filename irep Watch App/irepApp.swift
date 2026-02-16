ca7d074837eca86da24fbacc269825663d62f615
EdgarVMora <cansiller.ed@gmail.com>
2026-02-15 21:53:39 -0600

    Initial Commit

---
+++ irep Watch App/irepApp.swift
@@ -0,0 +1,17 @@
+//
+//  irepApp.swift
+//  irep Watch App
+//
+//  Created by Eddy :)  on 15/02/26.
+//
+
+import SwiftUI
+
+@main
+struct irep_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ irep Watch AppTests/irep_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  irep_Watch_AppTests.swift
+//  irep Watch AppTests
+//
+//  Created by Eddy :)  on 15/02/26.
+//
+
+import Testing
+@testable import irep_Watch_App
+
+struct irep_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
